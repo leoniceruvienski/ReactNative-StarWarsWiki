b74b5a7861afd49d548eae31f6f79d3e9f69fc81
Leonice <leonice@pop-os.localdomain>
2022-02-21 08:52:53 -0300

    Modificações em StarWarsWiki

---
+++ src/components/atoms/Logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {LogoImage} from './styles'
+import logoImage from '../../../../assets/p1.png'
+
+export const Logo = () => {
+    return (
+        <LogoImage source={logoImage}/>
+    )
+}
+
+
+  
